25bca8ce2d961660c42d7a23dfa91684767c2ac6
Oc1S <ocis.chan@gmail.com>
2024-11-23 15:35:38 +0800

    chore: turbo

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <ReactIcon></ReactIcon>
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite +r </h1>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
--- view/vite.config.ts
+++ view/vite.config.ts
@@ -7,14 +7,5 @@ export default defineConfig({
   build: {
     cssCodeSplit: false,
     outDir: '../out/view',
-    // rollupOptions: {
-    //   // 输出配置
-    //   output: {
-    //     // 输出的文件自定义命名
-    //     chunkFileNames: `js/[name]-[hash].${timeStamp}.js`,
-    //     entryFileNames: `js/[name]-[hash].${timeStamp}.js`,
-    //     assetFileNames: `[ext]/[name]-[hash].${timeStamp}.[text]`,
-    //   },
-    // },
   },
 })
